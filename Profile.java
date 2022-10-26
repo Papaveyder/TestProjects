ef9e606a412677dc9f871dff448a86a13d4c979d
Anton <papaveyder@gmail.com>
2022-10-26 14:51:15 +0300

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -28,39 +28,31 @@ public class Profile {
     }
 }
 		
-		Scanner scan = new Scanner(System.in); 
-		Student student = new Student(); 
-	
-		System.out.print("Hello There! Plese enter your Name: ");  String newName = scan.nextLine(); 
-		System.out.print("your Age: "); int newAge = scan.nextInt();  
+Scanner scan = new Scanner(System.in); 
+Student student = new Student(); 
+System.out.print("Hello There! Plese enter your Name: ");  String newName = scan.nextLine(); 
+System.out.print("your Age: "); int newAge = scan.nextInt();  
+
+student.setName(newName); student.setAge(newAge); 
 		
-		student.setName(newName); student.setAge(newAge); 
+boolean test = true; 
 		
-		boolean test = true; 
+while (test) {
 		
-		while (test) {
-		
-		if (student.getAge() <=0){
+if (student.getAge() <=0){
 		    
-		   System.out.println("Invalid Age! \n" + "Name: " + student.getName() + "\n" + "Age: " + student.getAge()); 
+	System.out.println("Invalid Age! \n" + "Name: " + student.getName() + "\n" + "Age: " + student.getAge()); 
 		   
-		   System.out.print("Please enter your age again: "); newAge = scan.nextInt();
+	System.out.print("Please enter your age again: "); newAge = scan.nextInt();
 		   
-		   student.setAge(newAge); 
-		   
-		}
-		
-		else {
-		    
-		    test = false; 
-		}
-		}
-		    System.out.println("Name: " + student.getName() + "\n" + "Age: " + student.getAge()); 
-		
-	}
-		
-		
-		
-		
-	}
+	student.setAge(newAge); 
+}
+
+else { 
+	test = false; 
+}
+ }
+	System.out.println("Name: " + student.getName() + "\n" + "Age: " + student.getAge()); 
+}
+ }
 
